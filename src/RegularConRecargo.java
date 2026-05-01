9faeefca01a7e80c1b2cfda9a9537a0845e50f5b
Rodry-095 <estebanldu2016@gmail.com>
2026-05-01 16:25:00 -0500

    Se avanza hasta loa 4ta clase

--- src/RegularConRecargo.java
+++ src/RegularConRecargo.java
@@ -1,2 +1,12 @@
-public class RegularConRecargo {
+public class RegularConRecargo extends EstudianteRegular {
+    private double recargo;
+
+    public void setRecargo(double r) {
+        if (r >= 0) recargo = r;
+    }
+
+    @Override
+    public double calcularPagoFinal() {
+        return super.calcularPagoFinal() + recargo;
+    }
 }
